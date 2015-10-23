3e72352a549c04a32b7080da73b76d63c5a88249
Sqpan2 <951514058@qq.com>
2015-10-23 23:55:52 +0800

    新建Maven项目：study.javabasic.thread
    新建项目study.javabasic.thread，用于java多线程的学习。2015年10月23日 23:55:45

---
+++ study.javabasic.thread/src/main/java/com/sqpan2/study/javabasic/thread/Test.java
@@ -0,0 +1,18 @@
+/**
+ * Company   : 
+ * Project   : 
+ * Copyright : Copyright (c) 2015
+ * 
+ * @author   : Sqpan2
+ * @date     : 2015年10月23日 下午11:46:29
+ * @version  : v1.0
+ */
+package com.sqpan2.study.javabasic.thread;
+
+public class Test {
+
+	public static void main(String args[]) {
+	
+		System.out.println("mian 方法");
+	}
+}
